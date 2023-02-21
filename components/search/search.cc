084294b04453f2e29dc1fe118996e312e3abd4d5
aandrejbur <aandrejbur@gmail.com>
2023-02-21 19:38:21 +0100

    Search engines updated

--- components/search/search.cc
+++ components/search/search.cc
@@ -28,9 +28,7 @@ bool DefaultSearchProviderIsGoogle(
       template_url_service->GetDefaultSearchProvider();
   if (!default_provider)
     return false;
-  return default_provider->GetEngineType(
-             template_url_service->search_terms_data()) ==
-         SearchEngineType::SEARCH_ENGINE_GOOGLE;
+  return true;
 }
 
 }  // namespace search
