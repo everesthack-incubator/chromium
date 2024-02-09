927e93db4ad51e6c0413cee7ccb3699e7ef1808d
aandrejbur <aandrejbur@gmail.com>
2024-02-09 14:25:17 +0100

    Remove unused extensions

--- chrome/browser/ui/browser_commands.cc
+++ chrome/browser/ui/browser_commands.cc
@@ -569,9 +569,8 @@ Browser* OpenTdnsInNewWindow(Profile* profile) {
       Browser::CreateParams(Browser::TYPE_NORMAL, profile, true);
   Browser* browser = Browser::Create(params);
   browser->tomiNet=true;
-  browser->window()->SetFocusToLocationBar(true);
   AddTabAt(browser, GURL("chrome-extension://mefjahdcgabaicceifopmjmlehnkfpbc/index.html"), -1, true);
-  
+  browser->window()->SetFocusToLocationBar(true);
   browser->window()->Show();
   
   
